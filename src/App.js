4f80ce7478744577a75f9cea092842a47d4d4118
jvv00 <jvv0006@gmail.com>
2024-01-29 15:47:12 +0900

    add BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import { Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./views";
 
 
 function App() {
   return (
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home/>}/>
       </Routes>
+      </BrowserRouter>
   );
 }
 
