3d67f19c13f4adba58b37073204f8143cb89455c
jvv00 <jvv0006@gmail.com>
2024-02-01 19:43:34 +0900

    polished사용, horizon-ui 가져와서 수정하기

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import Routers from './Routers'
 import { ChakraProvider,} from '@chakra-ui/react'
-import theme from './theme';
+import theme from './theme/theme';
+import { ThemeProvider } from 'styled-components';
 
 function App() {
   return (
     <ChakraProvider theme={theme}>
-      <Routers />
+      <ThemeProvider theme={theme}>
+        <Routers />
+      </ThemeProvider>
     </ChakraProvider>
+    
   )
 }
 
--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,18 +1,19 @@
 
 import { SearchIcon, SunIcon } from "@chakra-ui/icons";
-import { Box, ButtonGroup, Heading, IconButton} from "@chakra-ui/react";
+import { Box, Button, ButtonGroup, Heading, IconButton} from "@chakra-ui/react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
 import ContainerLg from "./Container";
+import { darken, lighten } from "polished";
 
 const Header = () => {
     return (
         <Box as="header" borderBottom={"1px solid"} borderColor={"#000000"}>
             <ContainerLg>
                 <Box display={["block", null, "flex"]} h={"100%"} alignItems={"center"} justifyContent={"space-between"}>
-                    <Heading fontSize={24} color={"brand.900"}>
+                    <Logo fontSize={24}>
                         <Link to="/">DashBoard</Link>
-                    </Heading>
+                    </Logo>
             <Nav>
                 <NavList>
                     <li><Link to="/">Main Dashboard</Link></li>
@@ -26,16 +27,28 @@ const Header = () => {
                 <IconButton aria-label="Search database" icon={<SearchIcon/>}/>
                 <IconButton aria-label="Light database" icon={<SunIcon/>}/>
             </IconBtnGroup></Box>
+            <ButtonGroup  spacing='6'>
+            <Button variant='outline'>Save</Button>
+            <Button variant="cancel">Cancel</Button>
+            </ButtonGroup>
             </ContainerLg>
         </Box>
     )
 }
 
+const Logo = styled(Heading)`
+    // color :  ${darken(0.2, '#ff0000')};
+    color: ${(props) => lighten(0.1,props.theme.colors.red[300])};
+    // color: ${(props) => (props).theme.colors.red[300]};
+`
+
 // const 변수이름 = styled.태그이름`속성 : 값;`
 const Nav = styled.nav`
+    width: 100%;
     height: 100%;
-    background: lightblue;
-    @media screen and (min-width:768px) {
+    background: ${lighten(0.1, 'lightblue')};
+    margin: 0 20px ;
+    @media screen and (min-width:380px) {
         height: 100px;
         
     }
@@ -57,7 +70,7 @@ const NavList = styled.ul`
     a {
         display: block;
     }
-    @media screen and (min-width:768px) {
+    @media screen and (min-width:380px) {
         flex-direction: row;
         
     }
