d762cfe149f997eed1362d7fa7534f001020bd06
jvv00 <jvv0006@gmail.com>
2024-02-06 18:07:52 +0900

    add lnb handlescroll (실패)

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -6,61 +6,18 @@ import { PiHamburger } from "react-icons/pi";
 import gsap from "gsap";
 import { useEffect, useState } from "react";
 
-// const Header = () => {
-
-//     const HandleScroll = () => {
-//         const scrollY = window.scrollY
-//         const hd = document.querySelector('#header')
-//         const navBelt = document.querySelector('.nav-belt__wrapper')
-//         const navBar = document.querySelector('.nav-bar__wrapper')
-//         const hdHeight = hd.offsetHeight // 헤더 높이
-//         // const swiperHeight = document.querySelector('.main-slide').offsetHeight // 슬라이드 높이
-//         const swiperHeight = document.querySelector('.main-slide')?.offsetHeight || 0; // 슬라이드 높이
-//         console.log(scrollY)
-//         console.log(swiperHeight - hdHeight)
-
-//         // if : 100px 이상 스크롤 되면 헤더에 배경색을 입힌다.
-//         if(scrollY > swiperHeight - hdHeight) {
-//             // gsap.to(요소, {옵션})
-//             gsap.to(navBar, {
-//                 backgroundColor: '#fff', duration: 0.5,
-//             });
-//             gsap.to(navBelt, {
-//                 backgroundColor: '#f6f7f8', duration: 0.5,
-//             });
-//             gsap.to(navBelt.querySelectorAll('button'), { color: '#000', duration: 0.5 });
-//         } else {
-//             // else : 100px 이하로 스크롤 되면 배경색을 없앤다.
-//             gsap.to(navBar, {
-//                 backgroundColor: '', duration: 0.5,
-//             });
-//             gsap.to(navBelt, {
-//                 backgroundColor: '', duration: 0.5,
-//             });
-//         }
-//     }   
-
-//     window.addEventListener('scroll', HandleScroll)
-
-//     return (
-//         <Box 
-//         id="header"
-//         as="header" 
-//         position={"fixed"} 
-//         top={0} 
-//         left={0} 
-//         right={0} 
-//         zIndex={1000} 
-//         bg="rgba(0,0,0,.1)" 
-//         backdropFilter={'saturate(180%) blur(5px)'}
-//         >
 const Header = () => {
     const [isScroll, setIsScroll] = useState(false)
-
     useEffect(() => {
         const handleScroll = () => {
             const navBeltHeight = document.querySelector('.nav-belt__wrapper')?.offsetHeight || 0
             const scrollPosition = window.pageYOffset || document.documentElement.scrollTop
+
+            if (navBeltHeight === 0) {
+                document.getElementById('header').style.top = '0'; // header의 top값을 0으로 설정
+                setIsScroll(true);
+                return; // 이후의 코드 실행하지 않고 종료
+            }
 
             if (scrollPosition > navBeltHeight) {
                 document.getElementById('header').style.top = '-32px'
@@ -75,6 +32,52 @@ const Header = () => {
         window.addEventListener('scroll', handleScroll)
         return () => window.removeEventListener('scroll', handleScroll)
     }, [])
+
+    const HandleScroll = () => {
+        const scrollY = window.scrollY
+        const navBelt = document.querySelector('.nav-belt__wrapper')
+        const navBar = document.querySelector('.nav-bar__wrapper')
+        const navBarHeight = navBar.offsetHeight // 헤더 높이
+        const swiperHeight = document.querySelector('.topCont')?.offsetHeight || 0 // 슬라이드 높이
+
+        // if : 100px 이상 스크롤 되면 헤더에 배경색을 입힌다.
+        if(scrollY > swiperHeight + navBarHeight) {
+            // gsap.to(요소, {옵션})
+            gsap.to(navBar, {
+                backgroundColor: '#fff',
+                boxShadow: '0 1px 4px 0 rgba(0, 0, 0, .2)',
+                duration: 0.5,
+            });
+            gsap.to(navBelt, {
+                backgroundColor: '#f6f7f8',
+                duration: 0.5,
+            });
+            gsap.to(navBelt.querySelectorAll('button'), {
+                color: '#000',
+                duration: 0.5
+            });
+            gsap.to(navBar.querySelectorAll('button, a'), {
+                color: '#000',
+                duration: 0.5
+            });
+        } else {
+            gsap.to(navBar, {
+                backgroundColor: '',
+                duration: 0.5,
+            });
+            gsap.to(navBelt, {
+                backgroundColor: '',
+                duration: 0.5,
+            });
+            gsap.to(navBar.querySelectorAll('button, a'), {
+                color: '',
+                duration: 0.5
+            });
+
+        }
+    }   
+
+    window.addEventListener('scroll', HandleScroll)
 
     return (
         <Box
